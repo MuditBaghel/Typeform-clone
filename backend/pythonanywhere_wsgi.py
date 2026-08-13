d3ee2780031dee3fe2cd9f9056ad3f63c1362375
{MuditBaghel} <{muditbaghel2023@gmail.com}>
2026-08-14 02:18:48 +0530

    Fix PythonAnywhere WSGI config with ASGI middleware and a2wsgi

--- backend/pythonanywhere_wsgi.py
+++ backend/pythonanywhere_wsgi.py
@@ -3,14 +3,16 @@ import sys
 import os
 
 # Add your project directory to the path
-path = '/home/YOUR_USERNAME/Typeform-clone/backend'
+path = '/home/mudit9999/Typeform-clone/backend'
 if path not in sys.path:
     sys.path.insert(0, path)
 
 # Set environment variables
-os.environ['DATABASE_URL'] = 'sqlite:////home/YOUR_USERNAME/Typeform-clone/backend/typeform.db'
+os.environ['DATABASE_URL'] = 'sqlite:////home/mudit9999/Typeform-clone/backend/typeform.db'
 
+# Import the FastAPI app
 from main import app
 
-# PythonAnywhere expects the application to be named 'application'
-application = app
+# Wrap ASGI app with WSGI middleware for PythonAnywhere
+from a2wsgi import ASGIMiddleware
+application = ASGIMiddleware(app)
